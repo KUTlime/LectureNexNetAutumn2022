76e6db6b9afc4d66f1d8a75dc1ec7af943103622
KUTlime <kutlime@gmail.com>
2022-10-24 14:15:05 +0200

    Update console application logic

--- NexNet.FileParserConsoleApp/Program.cs
+++ NexNet.FileParserConsoleApp/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace NexNet.FileParserConsoleApp;
+﻿using NexNet.Library.Extensions;
+using NexNet.Library.Parsing.File;
+using NexNet.Library.Parsing.File.Assembly;
+
+namespace NexNet.FileParserConsoleApp;
 
 public static class FileParserConsoleApp
 {
-    private static void Main(string[] args)
+    private static void Main(string[] inputArguments)
     {
-        Console.WriteLine("Hello words");
+        inputArguments.ForEach(path =>
+        {
+            Parser
+                .GetFilesFromDirectory(path)
+                .Where(f => f.IsCsv() && f.IsSingleFileAssembly())
+                .ForEach(Parser.PrintFileContent);
+        });
+        Console.ReadKey();
     }
 }
