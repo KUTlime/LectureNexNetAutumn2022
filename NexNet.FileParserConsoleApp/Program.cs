89cde999fcd310e0103468d25cff4c4cb96ed78f
KUTlime <kutlime@gmail.com>
2022-10-24 09:40:05 +0200

    Add console file parser stub

---
+++ NexNet.FileParserConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace NexNet.FileParserConsoleApp;
+
+public static class FileParserConsoleApp
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello words");
+    }
+}
